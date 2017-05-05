f338fb7a36ce3bea86ba46bda7a251d48deed352
Ali Emami <emamiali1993@gmail.com>
2017-05-05 10:11:33 -0700

    inital commit

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express'),
+    app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function index(req, res) {
+  res.sendFile('/index.html', {
+    root: __dirname
+  });
+});
+
+//=============
+//   Server
+//=============
+app.listen(3000, function(){
+  console.log('solar system is on 3000');
+});
