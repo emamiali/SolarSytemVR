fccb8cf84d8bd5658571e7c03444a967fd333cb5
Ali Emami <emamiali1993@gmail.com>
2017-05-09 19:21:46 -0700

    added saturns rings

--- public/app.js
+++ public/app.js
@@ -1,51 +1,88 @@
 console.log('3 ... 2 ... 1 ... liftoff');
 
-var scene = document.querySelector('a-scene');
+const scene = document.querySelector('a-scene');
 console.log(document.querySelector('a-scene'));
-for (var i = 0; i < 1000; i++) {
-  var obj = document.createElement('a-entity');
-  var position = getRandCoordz()
+//add 1000 objects for the asteroid belt
+for (let i = 0; i < 1000; i++) {
+  let rocks = document.createElement('a-entity');
+  let beltPosition = asteroidBelt();
 
-  obj.setAttribute('geometry', {
+  rocks.setAttribute('geometry', {
     primitive: 'octahedron',
-    radius: Math.random() / 5
+    radius: Math.random() / 2
   });
-  obj.setAttribute('material', {
+  rocks.setAttribute('material', {
     color: 'rgb(41, 13, 13)'
   });
-  obj.setAttribute('position', {
-    x: position[0],
-    y: 0,
-    z: position[1]
+  rocks.setAttribute('position', {
+    x: beltPosition[0],
+    y: 1,
+    z: beltPosition[1]
   });
-  scene.appendChild(obj);
-
-
+  scene.appendChild(rocks);
 }
 
-function getRandCoordz() {
-  var radius = 75;
-  var angle = Math.random() * Math.PI * 2;
-  var pt_radius_sq = Math.random() * radius * radius;
-  var rootOfRadius = Math.sqrt(pt_radius_sq);
+for ( let i = 0; i < 3000; i++) {
+  let belt =
+  document.createElement('a-entity');
+  let ringPosition = saturnRing();
+
+  belt.setAttribute('geometry', {
+    primitive: 'octahedron',
+    radius: Math.random() / 10
+  });
+  belt.setAttribute('material', {
+    color: 'rgb(69, 47, 47)'
+  });
+  belt.setAttribute('position', {
+    x: ringPosition[0],
+    y: 1,
+    z: ringPosition[1]
+  });
+  belt.setAttribute('rotation', '0 45 45');
+  scene.appendChild(belt);
+}
+
+//to get random coordinates for the asteroid belt
+function asteroidBelt() {
+  let radius = 75;
+  let angle = Math.random() * Math.PI * 2;
+  let pt_radius_sq = Math.random() * radius * radius;
+  let rootOfRadius = Math.sqrt(pt_radius_sq);
   if (rootOfRadius < 10){
-    rootOfRadius += 70
+    rootOfRadius += 70;
   }else if (rootOfRadius < 20) {
-    rootOfRadius += 60
+    rootOfRadius += 60;
   }else if (rootOfRadius < 30) {
-    rootOfRadius += 50
+    rootOfRadius += 50;
   }else if (rootOfRadius < 40) {
     rootOfRadius += 40;
   }else if (rootOfRadius < 50) {
     rootOfRadius += 30;
   }else if (rootOfRadius < 60) {
-    rootOfRadius += 20
+    rootOfRadius += 20;
   }else if (rootOfRadius < 70) {
-    rootOfRadius += 10
+    rootOfRadius += 10;
   }
-  var x = rootOfRadius * Math.cos(angle);
-  var z = rootOfRadius * Math.sin(angle);
-  let position = [x, z]
+  let x = rootOfRadius * Math.cos(angle);
+  let z = rootOfRadius * Math.sin(angle);
+  let beltPosition = [x, z]
 
-  return position
+  return beltPosition
+}
+
+function saturnRing(){
+  let radius = 10;
+  let angle = Math.random() * Math.PI * 2;
+  let pt_radius_sq = Math.random() * radius * radius;
+  let rootOfRadius = Math.sqrt(pt_radius_sq);
+  if (rootOfRadius < 5 ){
+    rootOfRadius += 5;
+  }
+  let x = rootOfRadius * Math.cos(angle);
+  let z = rootOfRadius * Math.sin(angle);
+  z += -114
+  let ringPosition = [x, z]
+  return ringPosition
+
 }
